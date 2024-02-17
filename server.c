e1426e4f9efe4a73c318fdc798ffc4368aeeac3f
Gergo Buzas <buzasgergo0615@gmail.com>
2024-02-17 18:57:38 +0100

    Finished basic server implementation

--- server.c
+++ server.c
@@ -2,11 +2,15 @@
 #include "stdlib.h"
 #include "sys/socket.h"
 #include "netinet/in.h"
+#include "unistd.h"
 
 #define SERVER_PORT 8080
+#define BUFFER_SIZE 1024
 #define MAX_NUM_OF_CONNECTIONS 5
 
 int main(){
+     char buffer[BUFFER_SIZE];
+
      // Step 1: Create the socket
      int server_socket = socket(AF_INET, SOCK_STREAM, 0);
      if (server_socket == -1){
@@ -37,17 +41,33 @@ int main(){
      }
      printf("socket listen succesful!\n\r");
 
-     // Step 4: accepting incoming connections and handling the request
+     // Step 4: accepting incoming connections --> Connection alive
      char response[] = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>";
-     printf("LISTENING ON PORT %d!!!", SERVER_PORT);
+     
      while(1) {
           int client_socket = accept(server_socket, (struct sockaddr *)&server_address, &server_addr_size);
           if (client_socket < 0){
                perror("accept unsuccesful");
                return -1;
           }
-          printf("Client accepted!...");
-          write(client_socket, response, sizeof(response));
+          printf("Client accepted!...\n\n\r");
+
+          // Step 5: Reading client request and sending response
+          int read_retval = read(client_socket, buffer, BUFFER_SIZE);
+          if (read_retval < 0){
+               perror("read unsuccesful");
+               return -1;
+          }
+          printf(buffer);
+          printf("\n\n\r");
+
+          int write_retval = write(client_socket, response, sizeof(response));
+          if (write_retval < 0){
+               perror("write unsuccesful");
+               return -1;
+          }
+
+          close(client_socket);
      }
 
      return 0;
